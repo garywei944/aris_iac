413f952cd8129e952b97be23db45f9a42afc6e72
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-12 12:52:32 -0400

    Update 2-2.c

--- Chap 2/Samples/2-2.c
+++ Chap 2/Samples/2-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int n, c = 0;
+	unsigned int n, c = 0;
 	scanf("%d", &n);
 	while (n != 1) {
 		if (n % 2 == 1)
@@ -10,5 +10,6 @@ int main() {
 			n /= 2;
 		c++;
 	}
+	printf("%d\n", c);
 	return 0;
 }
