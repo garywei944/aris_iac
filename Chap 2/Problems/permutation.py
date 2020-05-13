d4f8829dae0eef0ea9d9456d22c4dbb5d33de5d5
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-13 05:08:45 -0400

    Update permutation.py

--- Chap 2/Problems/permutation.py
+++ Chap 2/Problems/permutation.py
@@ -6,6 +6,6 @@
 # @Link    : https://github.com/garywei944
 
 for i in range(123, 330):
-    s = str(i) + str(2 * i) + str(3 * i)
+    s = str(i) + ' ' + str(2 * i) + ' ' + str(3 * i)
     if '1' in s and '2' in s and '3' in s and '4' in s and '5' in s and '6' in s and '7' in s and '8' in s and '9' in s:
-        print(i, i * 2, i * 3)
+        print(s)
